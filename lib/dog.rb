cd03c8fc46e3f3aad57f45047e06253bcd63819c
Wasanta <kwan_u_day@hotmail.com>
2021-12-14 12:12:48 -0500

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+    # setter method
+    def name=(dog_name)
+        # assign value
+        @this_dogs_name = dog_name
+    end
+    #  getter method
+    def name
+        # access the data and return
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+# call Dog#name= method with the value
+lassie.name = "Lassie"
+# call the Dog#name method
+puts lassie.name
